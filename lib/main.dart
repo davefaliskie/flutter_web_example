fc3806e3b1c737dd07b23d64632636ec543ce156
Dave Faliskie <davefaliskie@gmail.com>
2024-02-07 13:58:16 +0000

    Add basic flutter image example.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -23,7 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
 
   @override
@@ -31,39 +31,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Flutter Images On Web"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 30.0),
+              ClipOval(
+                child: Image.asset(
+                  "assets/images/Roads.png",
+                  height: 100,
+                ),
+              ),
+              const Text("Asset Image"),
+              const SizedBox(height: 30.0),
+
+              // Network Image
+              SizedBox(
+                height: 200,
+                child: Image.network(
+                  "https://1manstartup-youtube.s3.amazonaws.com/RoadsShareLink.jpg",
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) {
+                      return child;
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Icon(
+                      Icons.error,
+                      size: 50,
+                      color: Colors.red,
+                    );
+                  },
+                ),
+              ),
+              const Text("Network Image"),
+
+              const SizedBox(height: 30.0),
+
+              SizedBox(
+                height: 200,
+                child: CachedNetworkImage(
+                  imageUrl:
+                      "https://1manstartup-youtube.s3.amazonaws.com/RoadsShareLink.jpg",
+                  errorWidget: (context, url, error) => const Icon(
+                    Icons.error,
+                    size: 50,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              const Text("Cached Network Image"),
+              const SizedBox(height: 60.0),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
